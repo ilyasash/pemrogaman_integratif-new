fe040df26f3117639c8e5b8340acbb6ecc095efe
ilyasash <113879177+ilyasash@users.noreply.github.com>
2023-06-22 23:58:04 +0700

    Add files via upload

---
+++ sse/README.md
@@ -0,0 +1,3 @@
+![image](https://github.com/aloybm/pemrograman-integratif/assets/100351038/ab0ce31a-0138-44fc-80ea-f60a902e4c1b)
+![image](https://github.com/aloybm/pemrograman-integratif/assets/100351038/ab67e542-5dd2-4d82-b5e3-cf13fcccfaf1)
+![image](https://github.com/aloybm/pemrograman-integratif/assets/100351038/0c56f713-98a4-48c2-917a-e6fb7d4ed8b3)
---
+++ sse/index.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const app = express();
+
+const port = 3030;
+
+// Define Route
+app.get("/", (req, res) => {
+    console.log('client connected')
+
+    res.setHeader("Content-Type", "text/event-stream");
+    res.setHeader("Cache-Control", "no-cache");
+    res.setHeader("Connection", "keep-alive");
+    res.setHeader("Access-Control-Allow-Origin", "*");
+
+    const intervalId = setInterval(() => {
+        const date = new Date().toLocaleString();
+        res.write(`data: ${date}\n\n`)
+    }, 10000)
+
+    res.on('close', () => {
+        console.log('client closed connection')
+        clearInterval(intervalId);
+        res.end
+    })
+})
+
+app.listen(port, () => {
+    console.log('server is running')
+})
